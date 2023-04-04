195ba8fc9a72af345a22d1b6f3f779d681322496
OckhamOdyssey <sariego.nicolas@gmail.com>
2023-04-04 20:42:23 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,6 @@
+from flask import Flask, escape, request
+import socket
+app = Flask(__name__)
+@app.route('/')
+def get_hostname():
+    return "Aplicación servida desde hostname: "+socket.gethostname()
